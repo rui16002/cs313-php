dc5d792b208f1e3373ee5ecdc563bdf0a2be6c25
rui16002 <email.jruiz@yahoo.com>
2018-10-07 19:41:56 +0200

    ajax test

--- web/Prove03/form.php
+++ web/Prove03/form.php
@@ -9,12 +9,11 @@
 
 <?php
 // define variables and set to empty values
-$nameErr = $addressErr = $cityErr = $stateErr = $zipCodeErr = "";
-$name = $address = $city = $state = $zipCode = "";
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-if (empty($_POST["name"])) {
+  if (empty($_POST["name"])) {
     $nameErr = "Name is required";
   } else {
     $name = test_input($_POST["name"]);
@@ -24,86 +23,81 @@ if (empty($_POST["name"])) {
     }
   }
   
-if (empty($_POST["address"])) {
-    $addressErr = "Address is required";
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
   } else {
-    $address = test_input($_POST["address"]);
-    // check if address only contains letters, numbers and whitespace
-    if (!preg_match("/^[a-zA-Z0-9 ]*$/",$address)) {
-      $addressErr = "Only letters, numbers and white space allowed"; 
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
     }
   }
     
-  if (empty($_POST["city"])) {
-    $cityErr = "City is required";
+  if (empty($_POST["website"])) {
+    $website = "";
   } else {
-    $city = test_input($_POST["city"]);
-    // check if city only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$city)) {
-      $cityErr = "Only letters and white space allowed"; 
+    $website = test_input($_POST["website"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL"; 
     }
   }
 
-if (empty($_POST["state"])) {
-    $stateErr = "State is required";
+  if (empty($_POST["comment"])) {
+    $comment = "";
   } else {
-    $state = test_input($_POST["state"]);
-    // check if state only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$state)) {
-      $stateErr = "Only letters and white space allowed"; 
-    }
+    $comment = test_input($_POST["comment"]);
   }
 
-if (empty($_POST["zipCode"])) {
-    $zipCodeErr = "Zip Code is required";
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
   } else {
-    $zipCode = test_input($_POST["zipCode"]);
-    // check if zipCode only contains numbers
-    if (!preg_match("/^[0-9]*$/",$zipCode)) {
-      $zipCodeErr = "Only numbers allowed"; 
-    }
+    $gender = test_input($_POST["gender"]);
   }
+}
 
-  function test_input($data) {
+function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
-  }
 }
 ?>
 
-<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  <label for="name">Nombre</label>
-  <input type="text" value="<?php echo $name;?>" id="name" name="name">
-  <span class="error">* <?php echo $nameErr;?></span><br><br>
-  <label for="streetAddress">Dirección</label>
-  <input type="text" value="<?php echo $address;?>" id="address" name="address">
-  <span class="error">* <?php echo $addressErr;?></span><br><br>
-  <label for="city">Ciudad</label>
-  <input type="text" value="<?php echo $city;?>" id="city" name="city">
-  <span class="error">* <?php echo $cityErr;?></span><br><br>
-  <label for="state">Estado</label>
-  <input type="text" value="<?php echo $state;?>" id="state" name="state">
-  <span class="error">* <?php echo $stateErr;?></span><br><br>
-  <label for="zipCode">CP</label>
-  <input type="text" value="<?php echo $zipCode;?>" id="zipCode" name="zipCode">
-  <span class="error">* <?php echo $zipCodeErr;?></span><br><br>
-  <input type="submit" name="submit" value="Submit"> <br><br>
-  <p><span class="error">* Campo requerido</span></p>
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
 </form>
 
 <?php
 echo "<h2>Your Input:</h2>";
 echo $name;
 echo "<br>";
-echo $address;
+echo $email;
 echo "<br>";
-echo $city;
+echo $website;
 echo "<br>";
-echo $state;
+echo $comment;
 echo "<br>";
-echo $zipCode;
+echo $gender;
 ?>
 
 </body>
