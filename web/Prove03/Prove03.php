be85eb36663ee828bbac79e73de98d8bae37be56
rui16002 <email.jruiz@yahoo.com>
2018-10-07 18:01:41 +0200

    test ajax 2

--- web/Prove03/Prove03.php
+++ web/Prove03/Prove03.php
@@ -154,13 +154,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
   //----------------------------------------------------------------------------
     // collect value of input field
-    $purchasedItem = $_POST['purchasedItem'];
-    if (!empty($purchasedItem)) {
-        array_push($_SESSION["shopping_cart"], $purchasedItem)
+    if (!empty($_POST['purchasedItem'])) {
+        array_push($_SESSION["shopping_cart"], $_POST['purchasedItem'])
     }
-    $removedItem = $_POST['removedItem'];
-    if (!empty($removedItem)) {
-        array_splice($_SESSION["shopping_cart"],array_search($removedItem,$_SESSION["shopping_cart"]),1);
+    
+    if (!empty($_POST['removedItem'])) {
+        array_splice($_SESSION["shopping_cart"],array_search($_POST['removedItem'],$_SESSION["shopping_cart"]),1);
     }
     echo "<span>--------------------------------------------</span><br>";
     echo "<span>".$_SESSION["shopping_cart"]."</span><br>";
