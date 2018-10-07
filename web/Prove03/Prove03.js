282a0ffef8f1bdac7efe236332389c49abd862a1
rui16002 <email.jruiz@yahoo.com>
2018-10-07 16:28:21 +0200

    trying to fix set attribute of undefined

--- web/Prove03/Prove03.js
+++ web/Prove03/Prove03.js
@@ -65,11 +65,14 @@ function shopItem(itemName, itemDescription, itemImg, itemPrice)
  var name = document.createTextNode(itemName);
  var id = "purchased01";
 
-var col = document.createElement("div").setAttribute("class","col-sm-4").setAttribute("id",id);
+var col = document.createElement("div").setAttribute("class","col-sm-4");
+col.setAttribute("id",id);
 var panel = document.createElement("div").setAttribute("class","panel panel-default");
 var heading = document.createElement("div").setAttribute("class","panel-heading text-center").appendChild(name);
 var body = document.createElement("div").setAttribute("class","panel-body");
-var img = document.createElement("img").setAttribute("class", "img-responsive").setAttribute("alt", "Image").setAttribute("src", itemImg);
+var img = document.createElement("img").setAttribute("class", "img-responsive")
+img.setAttribute("alt", "Image");
+img.setAttribute("src", itemImg);
 var footer = document.createElement("div").setAttribute("class", "panel-footer");
 var frow = document.createElement("div").setAttribute("class", "row");
 var desc = document.createElement("div").setAttribute("class", "col-sm-12 description").appendChild(description);
