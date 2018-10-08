e9ef58355bd2876d6fa6c8943994884002afa046
rui16002 <email.jruiz@yahoo.com>
2018-10-08 19:40:55 +0200

    trying session once more

--- web/Prove03/session.php
+++ web/Prove03/session.php
@@ -1,47 +1,16 @@
-  <!--?php
+  <?php
     // collect value of input field
     if (!(empty($_POST["purchasedItem"]))) {
-        array_push($_SESSION["shopping_cart"], $_POST["purchasedItem"]); // Not working
+      echo $_POST["purchasedItem"];
+        //array_push($_SESSION["shopping_cart"], $_POST["purchasedItem"]); // Not working
     }
 
     if (!(empty($_POST["removedItem"]))) {
-      $item2remove = array_search($_POST["removedItem"],$_SESSION["shopping_cart"], true);
-      if (!($item2remove === false))
-      {
-        array_splice($_SESSION["shopping_cart"],$item2remove,1);
-      }
+      //$item2remove = array_search($_POST["removedItem"],$_SESSION["shopping_cart"], true);
+      //if (!($item2remove === false))
+      //{
+      //  array_splice($_SESSION["shopping_cart"],$item2remove,1);
+     // }
+     echo $_POST["removedItem"];
     }
-  ?-->
-
-<!DOCTYPE html>
-<html>
-<body>
-
-<div id="demo">
-
-<h2>The XMLHttpRequest Object</h2>
-
-<button type="button"
-onclick="loadDoc('session.php', myFunction)">Change Content
-</button>
-</div>
-
-<script>
-function loadDoc(url, cFunction) {
-  var xhttp;
-  xhttp=new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      cFunction(this);
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
-}
-function myFunction(xhttp) {
-  document.getElementById("demo").innerHTML =
-  xhttp.responseText;
-}
-</script>
-</body>
-</html>
+  ?>
