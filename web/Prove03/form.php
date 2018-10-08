f5c984c5fd90eba3352394c899b8104db573a9c4
rui16002 <email.jruiz@yahoo.com>
2018-10-08 13:08:50 +0200

    fix form

--- web/Prove03/form.php
+++ web/Prove03/form.php
@@ -22,6 +22,16 @@ if (empty($_POST["name"])) {
       $nameErr = "Only letters and white space allowed"; 
     }
   }
+  
+if (empty($_POST["address"])) {
+    $addressErr = "Address is required";
+  } else {
+    $address = test_input($_POST["address"]);
+    // check if address only contains letters, numbers and whitespace
+    if (!preg_match("/^[a-zA-Z0-9 ]*$/",$address)) {
+      $addressErr = "Only letters, numbers and white space allowed"; 
+    }
+  }
     
   if (empty($_POST["city"])) {
     $cityErr = "City is required";
@@ -43,28 +53,56 @@ if (empty($_POST["state"])) {
     }
   }
 
+if (empty($_POST["zipCode"])) {
+    $zipCodeErr = "Zip Code is required";
+  } else {
+    $zipCode = test_input($_POST["zipCode"]);
+    // check if zipCode only contains numbers
+    if (!preg_match("/^[0-9]*$/",$zipCode)) {
+      $zipCodeErr = "Only numbers allowed"; 
+    }
+  }
+  }
   function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
-  }
+
 }
 ?>
 
-<h2>PHP Form Validation Example</h2>
-<p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Name: <input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
-  <input type="submit" name="submit" value="Submit">  
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  <label for="name">Nombre</label>
+  <input type="text" value="<?php echo $name;?>" id="name" name="name">
+  <span class="error">* <?php echo $nameErr;?></span><br><br>
+  <label for="streetAddress">Dirección</label>
+  <input type="text" value="<?php echo $address;?>" id="address" name="address">
+  <span class="error">* <?php echo $addressErr;?></span><br><br>
+  <label for="city">Ciudad</label>
+  <input type="text" value="<?php echo $city;?>" id="city" name="city">
+  <span class="error">* <?php echo $cityErr;?></span><br><br>
+  <label for="state">Estado</label>
+  <input type="text" value="<?php echo $state;?>" id="state" name="state">
+  <span class="error">* <?php echo $stateErr;?></span><br><br>
+  <label for="zipCode">CP</label>
+  <input type="text" value="<?php echo $zipCode;?>" id="zipCode" name="zipCode">
+  <span class="error">* <?php echo $zipCodeErr;?></span><br><br>
+  <input type="submit" name="submit" value="Submit"> <br><br>
+  <p><span class="error">* Campo requerido</span></p>
 </form>
 
 <?php
 echo "<h2>Your Input:</h2>";
 echo $name;
 echo "<br>";
+echo $address;
+echo "<br>";
+echo $city;
+echo "<br>";
+echo $state;
+echo "<br>";
+echo $zipCode;
 ?>
 
 </body>
