c5e32e4932662704e618dbfd2766971e057feba1
rui16002 <email.jruiz@yahoo.com>
2018-10-08 20:20:47 +0200

    trying session once more

--- web/Prove03/session.php
+++ web/Prove03/session.php
@@ -1,15 +1,18 @@
   <?php
     // collect value of input field
     if (!(empty($_REQUEST["purchasedItem"]))) {
-        array_push($_SESSION["shopping_cart"], $_REQUEST["purchasedItem"]); // Not working
+        //array_push($_SESSION["shopping_cart"], $_REQUEST["purchasedItem"]); // Not working
+      echo json_encode($_REQUEST["purchasedItem"]);
     }
 
     if (!(empty($_REQUEST["removedItem"]))) {
+              echo json_encode($_REQUEST["removedItem"]);
       $item2remove = array_search($_REQUEST["removedItem"],$_SESSION["shopping_cart"], true);
-      if (!($item2remove === false))
+      //if (!($item2remove === false))
       {
-        array_splice($_SESSION["shopping_cart"],$item2remove,1);
+        //array_splice($_SESSION["shopping_cart"],$item2remove,1);
+
       }
-     echo json_encode($_SESSION["shopping_cart"]);
+     
     }
   ?>
