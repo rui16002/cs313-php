872d27c10fd7bb960fce7abf5d3d137b2cf4aa44
rui16002 <email.jruiz@yahoo.com>
2018-10-09 00:26:59 +0200

    trying session once more

--- web/Prove03/session.php
+++ web/Prove03/session.php
@@ -7,28 +7,25 @@ session_start();
   <title>Session</title>
 </head>
 <body>
-  <!--?php
+  <?php
+    $_SESSION["shopping_cart"] = array();
     // collect value of input field
     if (!(empty($_REQUEST["purchasedItem"]))) {
       $purchasedItem = $_REQUEST["purchasedItem"];
-      array_push($_SESSION["shopping_cart"], $purchasedItem);      
+      //array_push($_SESSION["shopping_cart"], $purchasedItem);      
     }
 
     if (!(empty($_REQUEST["removedItem"]))) {
       $removedItem = $_REQUEST["removedItem"];
-      $item2remove = array_search($removedItem,$_SESSION["shopping_cart"], true);
-      if (!($item2remove === false))
+      //$item2remove = array_search($removedItem,$_SESSION["shopping_cart"], true);
+      //if (!($item2remove === false))
       {
-        array_splice($_SESSION["shopping_cart"],$item2remove,1);
+     //   array_splice($_SESSION["shopping_cart"],$item2remove,1);
       }
-     echo json_encode($_SESSION["shopping_cart"]);
     }
-  ?-->
-  <?php
-  print_r($_SESSION);
-  // Set session variables
-$_SESSION["shopping_cart"] = array();
-echo "Session variables are set.";
-?>
+     echo json_encode($_SESSION["shopping_cart"]);
+     print_r($_SESSION);
+  ?>
+
 </body>
 </html>
