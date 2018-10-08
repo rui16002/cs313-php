797916522c7e424d7ddd8844453491c6688397a0
rui16002 <email.jruiz@yahoo.com>
2018-10-09 00:02:58 +0200

    trying session once more

--- web/Prove03/Prove03.php
+++ web/Prove03/Prove03.php
@@ -15,9 +15,6 @@ session_start();
 
 </head>
 <body>
-  <?php 
-    $_SESSION["shopping_cart"] = "";
-  ?>
 <div class="page-header">
  <img src="logo.jpg" class="img-responsive logo" alt="Logo">
 </div>
