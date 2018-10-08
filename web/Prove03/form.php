3b286d1d1d8fe581a464b5af349ba4c18bd8924e
rui16002 <email.jruiz@yahoo.com>
2018-10-08 12:58:07 +0200

    fix form

--- web/Prove03/form.php
+++ web/Prove03/form.php
@@ -9,11 +9,11 @@
 
 <?php
 // define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
+$nameErr = $addressErr = $cityErr = $stateErr = $zipCodeErr = "";
+$name = $address = $city = $state = $zipCode = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
+if (empty($_POST["name"])) {
     $nameErr = "Name is required";
   } else {
     $name = test_input($_POST["name"]);
@@ -23,44 +23,52 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
   }
   
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
+if (empty($_POST["address"])) {
+    $addressErr = "Address is required";
   } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
+    $address = test_input($_POST["address"]);
+    // check if address only contains letters, numbers and whitespace
+    if (!preg_match("/^[a-zA-Z0-9 ]*$/",$address)) {
+      $addressErr = "Only letters, numbers and white space allowed"; 
     }
   }
     
-  if (empty($_POST["website"])) {
-    $website = "";
+  if (empty($_POST["city"])) {
+    $cityErr = "City is required";
   } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL"; 
+    $city = test_input($_POST["city"]);
+    // check if city only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$city)) {
+      $cityErr = "Only letters and white space allowed"; 
     }
   }
 
-  if (empty($_POST["comment"])) {
-    $comment = "";
+if (empty($_POST["state"])) {
+    $stateErr = "State is required";
   } else {
-    $comment = test_input($_POST["comment"]);
+    $state = test_input($_POST["state"]);
+    // check if state only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$state)) {
+      $stateErr = "Only letters and white space allowed"; 
+    }
   }
 
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
+if (empty($_POST["zipCode"])) {
+    $zipCodeErr = "Zip Code is required";
   } else {
-    $gender = test_input($_POST["gender"]);
+    $zipCode = test_input($_POST["zipCode"]);
+    // check if zipCode only contains numbers
+    if (!preg_match("/^[0-9]*$/",$zipCode)) {
+      $zipCodeErr = "Only numbers allowed"; 
+    }
   }
-}
 
-function test_input($data) {
+  function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
+  }
 }
 ?>
 
@@ -70,20 +78,6 @@ function test_input($data) {
   Name: <input type="text" name="name" value="<?php echo $name;?>">
   <span class="error">* <?php echo $nameErr;?></span>
   <br><br>
-  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
-  Website: <input type="text" name="website" value="<?php echo $website;?>">
-  <span class="error"><?php echo $websiteErr;?></span>
-  <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-  <br><br>
-  Gender:
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
-  <span class="error">* <?php echo $genderErr;?></span>
-  <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
 
@@ -91,13 +85,6 @@ function test_input($data) {
 echo "<h2>Your Input:</h2>";
 echo $name;
 echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
 ?>
 
 </body>
