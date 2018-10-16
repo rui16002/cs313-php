a32795ac0c586f0e997b216830a7cc4157867e14
rui16002 <email.jruiz@yahoo.com>
2018-10-17 00:06:09 +0200

    print query results 4

--- web/Prove05/DBConnection.php
+++ web/Prove05/DBConnection.php
@@ -47,7 +47,7 @@ echo "Testing functions<br>";
 echo "<br>";
 $rows = getCustomers();
 foreach($rows as $x => $x_value) {
-  foreach($x_value as $y => $y_value) {
+  foreach($x as $y => $y_value) {
     echo "row=" . $x . ", Value=" . $y . " = " . $y_value;
     echo "<br>";
 }
