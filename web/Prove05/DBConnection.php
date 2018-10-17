bdd1698ea9494657dc424e266e5060f3a5b1d628
rui16002 <email.jruiz@yahoo.com>
2018-10-17 17:37:24 +0200

    db new functions

--- web/Prove05/DBConnection.php
+++ web/Prove05/DBConnection.php
@@ -40,12 +40,15 @@ return $rows;
 
 function getOrdersByNameDate($FirstName, $LastName, $OrderDate){
 global $db;
-$query = 'SELECT old.LastName, old.FirstName, old.Date, mi.Name, mi.Description, mi.Price, mi.Available
-FROM
+$query = '
+SELECT old.LastName, old.FirstName, old.OrderDate, mi.Name, mi.Description, mi.Price, mi.Available FROM
 (SELECT * FROM
 (SELECT * FROM Orderlists ol 
   INNER JOIN Customers c 
-  ON ol.CustomerID = c.CustomerID) cd INNER JOIN Orders o ON cd.OrderID = o.OrderID) old INNER JOIN Menuitems mi ON old.MenuitemID = mi.MenuitemID WHERE old.LastName=:LastName AND old.FirstName=:FirstName AND old.Date=:OrderDate';
+  ON ol.CustomerID = c.CustomerID) cd 
+  INNER JOIN Orders o ON cd.OrderID = o.OrderID) old 
+  INNER JOIN Menuitems mi ON old.MenuitemID = mi.MenuitemID 
+  WHERE old.LastName=:LastName AND old.FirstName=:FirstName AND old.Date=:OrderDate';
 $stmt = $db->prepare($query);
 $stmt->bindValue(':LastName', $LastName, PDO::PARAM_STR);
 $stmt->bindValue(':FirstName', $FirstName, PDO::PARAM_STR);
@@ -71,7 +74,7 @@ echo "Testing functions<br>";
 echo "<br>";
 print_r(getCustomersByName('Rubolino','Barbara'));
 echo "<br>";
-print_r(getOrdersByNameDate('Rubolino','Barbara','10/17/2018'));
+print_r(getOrdersByNameDate('Rubolino','Barbara','2018-10-15'));
 echo "<br>";
 print_r(getMenuitemsByTypeNameAvailable('Entrada','Nachos', true));
 
